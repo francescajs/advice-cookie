00e82a6d0ec35f8f3ee9c71933c16aadb94a8378
Francesca Song <francescajsong@gmail.com>
2019-03-09 21:04:10 -0800

    Updates

---
+++ precache-manifest.562184b126f47774fd049ecb9779b84a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e177dd35025869584a74",
+    "url": "/advice-cookie/static/js/runtime~main.620c5e30.js"
+  },
+  {
+    "revision": "b6b6237f5fbc35b75509",
+    "url": "/advice-cookie/static/js/main.798902a5.chunk.js"
+  },
+  {
+    "revision": "4bd30c3dc2c13bb1cf2d",
+    "url": "/advice-cookie/static/js/2.f4a775eb.chunk.js"
+  },
+  {
+    "revision": "b6b6237f5fbc35b75509",
+    "url": "/advice-cookie/static/css/main.a22e9442.chunk.css"
+  },
+  {
+    "revision": "636429f5d86e95c42c13c7e0e1afe1e3",
+    "url": "/advice-cookie/index.html"
+  }
+];
